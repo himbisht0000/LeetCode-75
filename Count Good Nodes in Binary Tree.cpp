0b186da7ad86b5cfa1526d60afcbdec294762a10
Himanshu SIngh Bisht <38493189+himbisht0000@users.noreply.github.com>
2024-03-30 23:59:13 +0530

    Create Count Good Nodes in Binary Tree.cpp

---
+++ Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve (TreeNode* root , int *count, int *max)
+    {   
+        int old_max = *max;
+        if(root == NULL)
+        return ;
+        // if val of current node is more than max in its path then count it
+        if(root->val >= *max)
+        {
+            *count = *count + 1 ;
+            *max = root->val;
+        }
+        solve(root->left, count,max);
+        solve(root->right,count, max);
+        // restore the old max value as ve are back tracking
+        *max = old_max;
+    }
+    int goodNodes(TreeNode* root) {
+        int count  = 0;
+        int max = root->val;
+        solve(root->left,&count,&max);
+        solve(root->right,&count,&max);
+
+        return count + 1;
+    }
+};
