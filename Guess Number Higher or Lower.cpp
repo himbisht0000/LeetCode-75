cc0fd578f990472f3c9ca363b9b5e84deb707d5c
Himanshu SIngh Bisht <38493189+himbisht0000@users.noreply.github.com>
2024-05-21 18:53:37 +0530

    Create Guess Number Higher or Lower.cpp

---
+++ Guess Number Higher or Lower.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int left = 1 , right = n;
+        int mid ;
+
+        while(left < right)
+        {
+            mid = left + ( right - left)/2;
+
+            if(guess(mid) == -1){
+                right = mid - 1;
+            }
+            else if(guess(mid) == 1)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+
+        return left;
+    }
+};
