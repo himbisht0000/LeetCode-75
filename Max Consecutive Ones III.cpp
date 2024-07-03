1311cc4a7e0f8df533cfa1abc4d2c0860ef71452
Himanshu SIngh Bisht <38493189+himbisht0000@users.noreply.github.com>
2024-07-03 22:13:31 +0530

    Update and rename Max Consecutive Ones III to Max Consecutive Ones III.cpp

--- Max Consecutive Ones III.cpp
+++ Max Consecutive Ones III.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        // consider 1's as land and 0's as lake and k as number of ladders
+
+        // starting and ending index of window
         int i = 0,j = 0;
+        // stores the size of current window
         int ans = 0;
         while(j < nums.size())
         {
+             // if current number is zero flip it
             if(nums[j] == 0)
             {
                 k--;
             }
+
+            // if we have flips k zeros save the size of window
             if( k == 0)
             {
                 ans = max(ans,j - i + 1);
             }
 
+            // if k < 0 that mena we flipped more zeros than k so we need to remove extra zero from left side of window
             if( k < 0)
             {
+                 // if removed element from window is zero value of k will increase
                 if(nums[i] == 0)
                 {
                     k++;
                 }
+                // reduce the size of window by move starting index towards right
                 i++;
             }
-
+            // move to next element in nums array
             j++;
         }
 
@@ -33,6 +41,7 @@ public:
         {
           ans = max(ans,j - i );
         }
+        // TC : O(N) , N is the size of nums array
         return ans;
     }
 };
