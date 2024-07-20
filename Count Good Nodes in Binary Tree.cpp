fcb89b7feac62bcb0636974ef611b2a78474401d
Himanshu SIngh Bisht <38493189+himbisht0000@users.noreply.github.com>
2024-07-20 09:21:03 +0530

    Update Count Good Nodes in Binary Tree.cpp

--- Count Good Nodes in Binary Tree.cpp
+++ Count Good Nodes in Binary Tree.cpp
@@ -11,30 +11,29 @@
  */
 class Solution {
 public:
-    void solve (TreeNode* root , int *count, int *max)
+    void solve (TreeNode* root , int *count, int max)
     {   
-        int old_max = *max;
+        
         if(root == NULL)
         return ;
         // if val of current node is more than max in its path then count it
-        if(root->val >= *max)
+        if(root->val >= max)
         {
             *count = *count + 1 ;
-            *max = root->val;
+            max = root->val;
         }
         solve(root->left, count,max);
         solve(root->right,count, max);
         // restore the old max value as ve are back tracking
-        *max = old_max;
+        
     }
     int goodNodes(TreeNode* root) {
         int count  = 0;
         int max = root->val;
-        solve(root->left,&count,&max);
-        solve(root->right,&count,&max);
+        solve(root,&count,max);
         
         //root will always be good node so add 1 to our answer
         //TC : O(N) where N is the number of node in binary tree
-        return count + 1;
+        return count ;
     }
 };
