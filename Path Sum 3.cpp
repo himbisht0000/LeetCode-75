c353e38623a504b44e114ee6cfbe8b54b73f8669
Himanshu SIngh Bisht <38493189+himbisht0000@users.noreply.github.com>
2024-07-24 21:29:17 +0530

    Update Path Sum 3.cpp

--- Path Sum 3.cpp
+++ Path Sum 3.cpp
@@ -1,28 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     void solve(TreeNode* root, long long &ans , long long current_sum, map<long long,long long>& target, int targetSum)
     {
+        //Base condition
         if(root == NULL)
         return;
 
-        current_sum  = (long long)current_sum + (long long)root->val;
+        //update the current sum 
+        current_sum  += (long long)root->val;
 
+        //Check if prefix sum exists in map or not
         if(target.find(current_sum - targetSum) != target.end() )
         {
             ans += target[current_sum - targetSum];
         }
 
+        // Update the entry of new sum in map
         if(target.find(current_sum) == target.end())
         {
             cout<<"current_sum"<<current_sum<<endl;
@@ -32,19 +25,19 @@ public:
         {
             target[current_sum]++;
         }
-
+        
+        // traversing the tree 
         solve(root->left,ans,current_sum,target,targetSum);
         solve(root->right,ans,current_sum,target,targetSum);
         
         // reduce the count of frequency
-        if(target[current_sum] >= 1)
         target[current_sum]--;
 
     }
     int pathSum(TreeNode* root, int targetSum) {
         map<long long,long long> target;
         long long ans = 0;
-        // if current sum exactly matches the targetSum then in map thier should be a wntry of zero
+        // if current sum exactly matches the targetSum then in map thier should be a entry of zero
         target[0] = 1;
         solve(root,ans,0,target,targetSum);
 
