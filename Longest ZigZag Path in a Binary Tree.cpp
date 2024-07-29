58f6a576eeefe7af77282b4665c37e3298af5d90
Himanshu SIngh Bisht <38493189+himbisht0000@users.noreply.github.com>
2024-07-29 20:10:42 +0530

    Update Longest ZigZag Path in a Binary Tree.cpp

--- Longest ZigZag Path in a Binary Tree.cpp
+++ Longest ZigZag Path in a Binary Tree.cpp
@@ -1,31 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    int answer = 0;
+    // store the maximum zigzag path
+    int output = 0;
     void solve(TreeNode* root , char dir, int path)
     {
+        // base condition if node is null simply return
         if(root == NULL)
         return ;
         
-        answer = max(answer,path);
+        //store max path we have found till now
+        output = max(output,path);
+       
+        //check if we have to go left or right
         if(dir == 'L')
         {
+            // if we go to left and increasing length of zigzag path
             solve(root->left,'R', path + 1);
+            // if we go to right we simply pass 1 as zigzag path will restart
             solve(root->right,'L', 1);
         }
         else
         {
+            // if we go to left we simply pass 1 as zigzag path will restart
             solve(root->left,'R', 1);
+            // if we go to right and increasing length of zigzag path
             solve(root->right,'L', path + 1);
         }
 
@@ -36,9 +35,10 @@ public:
         if(root == NULL)
         return 0;     
 
+        // find longest zigzag path by going firts left then right
         solve(root , 'L' , 0);
         solve(root, 'R' , 0);
 
-        return answer;
+        return output;
     }
 };
